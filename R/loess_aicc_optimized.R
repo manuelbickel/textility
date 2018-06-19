d075db4ab36435ee03e24a2e24fcc67cc1f619ab
manuelbickel <manuel.bickel@posteo.de>
2018-06-19 21:07:24 +0200

    update aicc loess

--- R/loess_aicc_optimized.R
+++ R/loess_aicc_optimized.R
@@ -10,49 +10,48 @@
 #' @param dependent_var The data column of the values to be fitted by \code{loess}.
 #' @param span_interval The span interval to be considered for AICC optimization of the loess span. By default \code{c(.1,.9)}.
 #'
-#' @return The original \code{x} with tow additional columns named \code{"aicc_loees_value"} and \code{"aicc_span"}
+#' @return The original \code{x} with tow additional columns named \code{"aicc_loess_value"} and \code{"aicc_span"}
 #'         Entries in aicc_span are identical for an individual group.
 #'         The entries in the additional columns that correspond to excluded groups or independent variables appear as \code{NA}.
 #' @export
 #'
 #' @examples
-
-# the following example show how aicc loess
-# produces a fit that is more sensible to the data
-# in comparison to standard loess using a span of 0.75
-# if such less smooth appearance makes sense is, of course, case specific
-
-years <- 1990:2010
-set.seed(42)
-topics <- c(rep(1, length(years)), rep(2, length(years)))
-values1 <- jitter(rnorm(length(years), mean = 0.5, sd = .25), amount = pi)
-values2 <- jitter(rnorm(length(years), mean = 0.25, sd = 0), amount = exp(1))
-
-data <- data.table(year = years, topic = topics, value = c(values1, values2))
-data <- loess_aicc_optimized(x = data
-                             , independent_var = "year"
-                             , independent_var_range = 1991:2009
-                             , dependent_var = "value"
-                             , groups = "topic"
-                             , group_subset = NULL
-                             , span_interval = NULL
-                             , degree = 2
-                             , family = "gaussian"
-                             , normalize = TRUE)
-# check the new content
-data
-
-plot(data[topic == 1,year], data[topic == 1,value], col = 1, ylim = c(min(data$value), max(data$value)))
-#default span is set to 0.75 in loess; this is explicitly included in below call for the sake of clarity
-lines(data[topic == 1,year], predict(loess(value ~ year, span = 0.75, data = data[topic == 1,])), col = 1, lty = "dashed")
-lines(data[topic == 1,year], data[topic == 1,aicc_loees_value], col = 1, lty = "solid")
-legend(x = 1990, y = -1, c("solid: loess_aicc\ndashed: loess_standard"))
-
-plot(data[topic == 2,year], data[topic == 2,value], col = 1)
-lines(data[topic == 2,year], predict(loess(value ~ year, data = data[topic == 2,])), col = 1, lty = "dashed")
-lines(data[topic == 2,year], data[topic == 2,aicc_loees_value], col = 1, lty = "solid")
-legend(x = 1990, y = 2, c("solid: loess_aicc\ndashed: loess_standard"))
-
+#'
+#' # the following example show how aicc loess
+#' # produces a fit that is more sensible to the data
+#' # in comparison to standard loess using a span of 0.75
+#' # if such less smooth appearance makes sense is, of course, case specific
+#'
+#' years <- 1990:2010
+#' set.seed(42)
+#' topics <- c(rep(1, length(years)), rep(2, length(years)))
+#' values1 <- jitter(rnorm(length(years), mean = 0.5, sd = .25), amount = pi)
+#' values2 <- jitter(rnorm(length(years), mean = 0.25, sd = 0), amount = exp(1))
+#'
+#' data <- data.table(year = years, topic = topics, value = c(values1, values2))
+#' data <- loess_aicc_optimized(x = data
+#'                              , independent_var = "year"
+#'                              , independent_var_range = 1991:2009
+#'                              , dependent_var = "value"
+#'                              , groups = "topic"
+#'                              , group_subset = NULL
+#'                              , span_interval = NULL
+#'                              , degree = 2
+#'                              , family = "gaussian"
+#'                              , normalize = TRUE)
+#' # check the new content
+#' data
+#'
+#' plot(data[topic == 1,year], data[topic == 1,value], col = 1, ylim = c(min(data$value), max(data$value)))
+#' #default span is set to 0.75 in loess; this is explicitly included in below call for the sake of clarity
+#' lines(data[topic == 1,year], predict(loess(value ~ year, span = 0.75, data = data[topic == 1,])), col = 1, lty = "dashed")
+#' lines(data[topic == 1,year], data[topic == 1,aicc_loess_value], col = 1, lty = "solid")
+#' legend(x = 1990, y = -1, c("solid: loess_aicc\ndashed: loess_standard"))
+#'
+#' plot(data[topic == 2,year], data[topic == 2,value], col = 1)
+#' lines(data[topic == 2,year], predict(loess(value ~ year, data = data[topic == 2,])), col = 1, lty = "dashed")
+#' lines(data[topic == 2,year], data[topic == 2,aicc_loess_value], col = 1, lty = "solid")
+#' legend(x = 1990, y = 2, c("solid: loess_aicc\ndashed: loess_standard"))
 
 loess_aicc_optimized <- function(x
                                  , independent_var = NULL, independent_var_range = NULL
@@ -70,7 +69,7 @@ loess_aicc_optimized <- function(x
 
   if (is.null(span_interval)) {
     span_interval <- c(.1,.9)
-    message("No span interval sepcified. Interval set to c(.1, .9).")
+    message("No span interval specified. Interval set to c(.1, .9).")
   }
 
   if (is.null(group_subset)) {
@@ -125,7 +124,7 @@ loess_aicc_optimized <- function(x
                                             , span = ", span_optimum, # <<< optimized span
                                             ",degree = ", degree, ", family = \"", family, "\", normalize = ", normalize,")")))
     #update data with loess data
-    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loees_value:= predict(loess_model_optimum)]
+    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loess_value:= predict(loess_model_optimum)]
     x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_span:= rep(span_optimum, nrow(loess_data))]
   }
   return(x)
