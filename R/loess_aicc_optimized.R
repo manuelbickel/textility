ff49a5c3afdf52f43ab5a49593cb6e1f3e1d7dfe
manuelbickel <manuel.bickel@posteo.de>
2018-07-17 21:12:56 +0200

    add output = model to loess aicc

--- R/loess_aicc_optimized.R
+++ R/loess_aicc_optimized.R
@@ -2,33 +2,54 @@
 #' Fitting of AICC optimized loess (groupwise)
 #'
 #' Given a data.table, three columns may be specified as dependent variable, independent variable, and group respectively
-#' to fit an AICC-optimized-span loess model. The information about selected span and predicted values is added as new columns per group.
+#' to fit an AICC-optimized-span loess model. The predicted values and upper/lower values of confidence band are added as new columns per group.
+#' Alternatively the function may simply return the full optimized model objects per group.
+#'
+#' For the default confidence interval of \code{0.95}, upper / lower values of confidence band
+#' are calculated as \code{model$fit +/- qt(0.25,model$df)*model$se}.
+#' This is only an estimation of the true confidence interval based on the standard error, see:
+#' https://stackoverflow.com/questions/22717930/how-to-get-the-confidence-intervals-for-lowess-fit-using-r
 #'
 #' @param x A \code{data.table} with data in long format (use \code{data.table::melt} if your data is in wide format).
-#' @param group_subset By default \code{loess} is fitted for all groups. A subset may be specified as \code{character} vector.
+#' @param independent_var Name of column containing the independent variable.
 #' @param independent_var_range By default \code{loess} is fitted for the full range of the independent variable. A limited range may be specified here, usually an \code{integer} vector.
 #' @param dependent_var The data column of the values to be fitted by \code{loess}.
+#' @param groups Name of the column containing the names of the groups.
+#' @param group_subset By default \code{loess} is fitted for all groups. A subset may be specified as \code{character} vector.
 #' @param span_interval The span interval to be considered for AICC optimization of the loess span. By default \code{c(.1,.9)}.
+#' @param degree Degree parameter passed to \code{loess}. By default \code{2}.
+#' @param family Family parameter passed to \code{loess}. By default \code{"gaussian"}.
+#' @param normalize Normalize parameter passed to \code{loess}. By default \code{"TRUE"}.
+#' @param confidence_interval The confidence interval for calculating upper/lower values for plotting confidence band. By default \code{0.95}.
+#'                            See detail section for details on calculation.
+#' @param output "fitted_values" results in new columns added to \code{x} containing fitted values for plotting, etc.
+#'               "model" results in return of the full optimized model objects per group.
 #'
-#' @return The original \code{x} with tow additional columns named \code{"aicc_loess_value"} and \code{"aicc_span"}
-#'         Entries in aicc_span are identical for an individual group.
-#'         The entries in the additional columns that correspond to excluded groups or independent variables appear as \code{NA}.
+#' @return The original \code{x} with three additional columns named \code{"aicc_loess_fit"} and \code{"aicc_loess_lwr"}, \code{"aicc_loess_upr"}.
+#'         The two latter columns inlcude the upper and lower values of the specified confidence interval.
+#'         The entries in these additional columns that correspond to excluded groups or independent variables appear as \code{NA_real_}.
+#'
+#'         If \code{output = "model"} a two column \code{data.table} is returned containing the group names and associated fitted model object per row.
 #' @export
 #'
 #' @examples
 #'
-#' # the following example show how aicc loess
+#'
+#' # the following example shows how aicc loess
 #' # produces a fit that is more sensible to the data
 #' # in comparison to standard loess using a span of 0.75
 #' # if such less smooth appearance makes sense is, of course, case specific
 #'
+#'
+#' library(data.table)
 #' years <- 1990:2010
 #' set.seed(42)
 #' topics <- c(rep(1, length(years)), rep(2, length(years)))
 #' values1 <- jitter(rnorm(length(years), mean = 0.5, sd = .25), amount = pi)
 #' values2 <- jitter(rnorm(length(years), mean = 0.25, sd = 0), amount = exp(1))
-#'
 #' data <- data.table(year = years, topic = topics, value = c(values1, values2))
+#'
+#' # first option: output = "fitted_values"
 #' data <- loess_aicc_optimized(x = data
 #'                              , independent_var = "year"
 #'                              , independent_var_range = 1991:2009
@@ -38,20 +59,41 @@
 #'                              , span_interval = NULL
 #'                              , degree = 2
 #'                              , family = "gaussian"
-#'                              , normalize = TRUE)
+#'                              , normalize = TRUE
+#'                              , output = "fitted_values")
 #' # check the new content
-#' data
+#' print(data)
 #'
 #' plot(data[topic == 1,year], data[topic == 1,value], col = 1, ylim = c(min(data$value), max(data$value)))
 #' #default span is set to 0.75 in loess; this is explicitly included in below call for the sake of clarity
-#' lines(data[topic == 1,year], predict(loess(value ~ year, span = 0.75, data = data[topic == 1,])), col = 1, lty = "dashed")
-#' lines(data[topic == 1,year], data[topic == 1,aicc_loess_value], col = 1, lty = "solid")
-#' legend(x = 1990, y = -1, c("solid: loess_aicc\ndashed: loess_standard"))
+#' lines(data[topic == 1,year], predict(loess(value ~ year, span = 0.75, data = data[topic == 1,])), col = 1)
+#' lines(data[topic == 1,year], data[topic == 1,aicc_loess_fit], col = 2)
+#' # add aicc loess confidence intervals
+#' lines(data[topic == 1,year], data[topic == 1,aicc_loess_lwr], col = 2, lty = "dashed")
+#' lines(data[topic == 1,year], data[topic == 1,aicc_loess_upr], col = 2, lty = "dashed")
+#' legend(x = 1990, y = max(data[topic == 1,value]), c("loess_aicc (dashed: conf. intv.)", "loess_standard"), fill = c(2,1))
 #'
 #' plot(data[topic == 2,year], data[topic == 2,value], col = 1)
-#' lines(data[topic == 2,year], predict(loess(value ~ year, data = data[topic == 2,])), col = 1, lty = "dashed")
-#' lines(data[topic == 2,year], data[topic == 2,aicc_loess_value], col = 1, lty = "solid")
-#' legend(x = 1990, y = 2, c("solid: loess_aicc\ndashed: loess_standard"))
+#' lines(data[topic == 2,year], predict(loess(value ~ year, data = data[topic == 2,])), col = 1)
+#' lines(data[topic == 2,year], data[topic == 2,aicc_loess_fit], col = 2)
+#' legend(x = 1990, y = max(data[topic == 2,value]), c("loess_aicc", "loess_standard"), fill = c(2,1))
+#'
+#' # second option: output = "model"
+#' models = loess_aicc_optimized(x = data
+#'                               , independent_var = "year"
+#'                               , dependent_var = "value"
+#'                               , groups = "topic"
+#'                               , output = "model")
+#' models
+#' # group model_aicc_loess
+#' # 1:     1          <loess>
+#' # 2:     2          <loess>
+#' class(models[1,2])
+#' # [1] "data.table" "data.frame"
+#' class(models[1,2][[1]])
+#' # [1] "list"
+#' class(models[1,2][[1]][[1]])
+#' # [1] "loess"
 
 loess_aicc_optimized <- function(x
                                  , independent_var = NULL, independent_var_range = NULL
@@ -60,7 +102,9 @@ loess_aicc_optimized <- function(x
                                  , span_interval = NULL
                                  , degree = 2
                                  , family = "gaussian"
-                                 , normalize = TRUE) {
+                                 , normalize = TRUE
+                                 , confidence_interval = 0.95
+                                 , output = c("model")) {
 
   #INPUT CHECKS
   stopifnot(!is.null(independent_var))
@@ -108,8 +152,18 @@ loess_aicc_optimized <- function(x
     return(log(sigma2) + 1 + (2 * (trace + 1)) / (n - trace - 2))
   }
 
+  if (output == "fitted_values") {
   # initialize new columns
-  x <- cbind(x, aicc_loess_value = rep(NA_real_, nrow(x)), aicc_span = rep(NA_real_, nrow(x)))
+  x <- cbind(x
+             , aicc_loess_fit = rep(NA_real_, nrow(x))
+             , aicc_loess_lwr = rep(NA_real_, nrow(x))
+             , aicc_loess_upr = rep(NA_real_, nrow(x))
+             )
+  } else {
+  #  https://stackoverflow.com/questions/51332754/how-to-initialize-a-data-table-column-with-empty-lists-and-loop-over-it
+  y <- data.table(group = group_subset, model_aicc_loess = vector(length(group_subset), mode="list"))
+  }
+
   # optimiize
   for (g in group_subset) {
     loess_data <- x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, .SD, .SDcols = c(independent_var, groups, dependent_var)]
@@ -122,10 +176,22 @@ loess_aicc_optimized <- function(x
     span_optimum <- optimize(function_to_optimize, interval = span_interval)$minimum
     loess_model_optimum <- eval(parse(text = paste0("loess(",dependent_var, " ~ ", independent_var,", data = loess_data
                                             , span = ", span_optimum, # <<< optimized span
-                                            ",degree = ", degree, ", family = \"", family, "\", normalize = ", normalize,")")))
+                                            ",degree = ", degree, ", family = \"", family, "\", normalize = ", normalize, ")")))
+    if (output == "fitted_values") {
     #update data with loess data
-    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loess_value:= predict(loess_model_optimum)]
-    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_span:= rep(span_optimum, nrow(loess_data))]
+    fit = predict(loess_model_optimum, se=T)
+    # https://stackoverflow.com/questions/22717930/how-to-get-the-confidence-intervals-for-lowess-fit-using-r
+    confidence_quantile = 1-((1-confidence_interval)/2)
+    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loess_fit:= predict(loess_model_optimum)]
+    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loess_lwr:= fit$fit - qt(confidence_quantile,fit$df)*fit$se]
+    x[eval(parse(text = groups)) == g & eval(parse(text = independent_var)) %in% independent_var_range, aicc_loess_upr:= fit$fit + qt(confidence_quantile,fit$df)*fit$se]
+    } else {
+    y[group == g, model_aicc_loess:= list(list(loess_model_optimum))]
+    }
   }
+  if (output == "fitted_values") {
   return(x)
+  } else {
+  return(y)
+  }
 }
